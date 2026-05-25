fd4efc14d262994b6ae8569806e011e3ec13c5a7
srita2003 <srita201326@gmail.com>
2026-05-25 15:50:45 +0530

    Added Terraform outputs

--- deployment-environments/sandbox/outputs.tf
+++ deployment-environments/sandbox/outputs.tf
@@ -1,3 +1,27 @@
 output "project_name" {
   value = "aws-terravault-grid"
 }
+
+output "vpc_id" {
+  value = module.network_fabric.vpc_id
+}
+
+output "public_subnet_id" {
+  value = module.network_fabric.public_subnet_id
+}
+
+output "private_subnet_id" {
+  value = module.network_fabric.private_subnet_id
+}
+
+output "ec2_instance_id" {
+  value = module.compute_mesh.ec2_id
+}
+
+output "database_id" {
+  value = module.database_layer.database_name
+}
+
+output "artifact_bucket" {
+  value = module.storage_vault.artifact_bucket
+}
